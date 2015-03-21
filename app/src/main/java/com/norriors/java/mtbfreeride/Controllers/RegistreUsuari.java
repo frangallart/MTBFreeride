37150184a8534e2be2f1758f6730d435d3733a86
Ruben <rgarcia@infobosccoma.net>
2015-03-21 21:01:37 +0100

    -Acció de fer/escollir fotografia implementada -Afegit fitxer de menú contextual -Afegida unes imatges -Afegit permisos External Storage i Camera2 en el Manifest -Afegit fitxer d'Strings pel layout registre-usuari

--- app/src/main/java/com/norriors/java/mtbfreeride/Controllers/RegistreUsuari.java
+++ app/src/main/java/com/norriors/java/mtbfreeride/Controllers/RegistreUsuari.java
@@ -1,21 +1,45 @@
 package com.norriors.java.mtbfreeride.Controllers;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
+import android.view.ContextMenu;
 import android.view.Gravity;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.LinearLayout;
 
 import com.norriors.java.mtbfreeride.R;
 
 /**
  * Classe RegistreUsuari
+ * <p/>
+ * Ens permet registrar un nou usuari al sistema
+ * <p/>
+ * L'usuari ha d'escollir una imatge de la galeria o de la càmara i introduïrs diversos paràmetres:
+ * Nom usuari, Contrassenya, Nom, Cognoms i email
+ * Un cop fet clic al botó registrar, si les dades són vàlides, les dades es traspassen amb JSON fins
+ * A una base de dades MySQL allotjada en un servidor extern.
  */
 public class RegistreUsuari extends ActionBarActivity {
 
+    private static int RESULT_LOAD_IMAGE = 1;
+    private static int TAKE_PHOTO_CODE = 100;
+
     private MLRoundedImageView roundedImageView;
+
     private LinearLayout linearImage;
+
+    private ImageTool imgTool;
+
+    private byte[] imatgeArray;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +48,7 @@ public class RegistreUsuari extends ActionBarActivity {
 
 
         //Instanciem el linear de la vista que contindrà la imatge
-         linearImage = (LinearLayout) findViewById(R.id.linearImage);
+        linearImage = (LinearLayout) findViewById(R.id.linearImage);
 
         //Instanciem un objecte del tipus MLRoundedImageView(Exten de ImageView) que ens permetrà tenir un imageView Rodó
         roundedImageView = new MLRoundedImageView(this);
@@ -32,6 +56,13 @@ public class RegistreUsuari extends ActionBarActivity {
 
         //Assignem la imatge al imageView
         roundedImageView.setImageResource(R.drawable.img_splash_screen);
+        roundedImageView.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                registerForContextMenu(roundedImageView);
+            }
+        });
 
         //Afegim l'ImageView al Linear
         linearImage.addView(roundedImageView);
@@ -40,6 +71,106 @@ public class RegistreUsuari extends ActionBarActivity {
         roundedImageView.getLayoutParams().width = 300;
         //Paràmetre per indicar la gravetat
         linearImage.setGravity(Gravity.CENTER_HORIZONTAL);
+    }
+
+
+    /**
+     * Menú contextual que ens permetrà accedir a la galeria o la càmara
+     *
+     * @param menu
+     * @param v
+     * @param menuInfo
+     */
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v,
+                                    ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        menu.setHeaderTitle("Opcions multimèdia");
+        menu.setHeaderIcon(R.drawable.ic_action_crop);
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_contextual, menu);
+
+    }
+
+    /**
+     * Events dels Items del menú contextual
+     *
+     * @param item
+     * @return
+     */
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+
+        final AdapterView.AdapterContextMenuInfo info =
+                (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+
+        switch (item.getItemId()) {
+            case R.id.action_galeria:
+                accessGaleria();
+                return true;
+            case R.id.action_camara:
+                accessCamara();
+                return true;
+
+            default:
+                return super.onContextItemSelected(item);
+        }
+    }
+
+    public void accessGaleria() {
+        Intent i = new Intent(
+                Intent.ACTION_PICK,
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+        startActivityForResult(i, RESULT_LOAD_IMAGE);
+    }
+
+
+    private void accessCamara() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(takePictureIntent, TAKE_PHOTO_CODE);
+    }
+
+    /**
+     * Mètode que s'exeuta en retornar a aquesta activitat
+     * En auqest cas, comprova si s'ha carregat una imatge de la galeria de fotos o de la
+     * càmara per així poder assignar-la com a fons del botó.
+     * @param requestCode
+     * @param resultCode
+     * @param data
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if ((requestCode == RESULT_LOAD_IMAGE || requestCode == TAKE_PHOTO_CODE)&& resultCode == RESULT_OK && null != data) {
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = {MediaStore.Images.Media.DATA};
+            String result;
+            Cursor cursor = getContentResolver().query(selectedImage,
+                    filePathColumn, null, null, null);
+            if (cursor == null) {
+                result = selectedImage.getPath();
+            } else {
+                cursor.moveToFirst();
+                int idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
+                result = cursor.getString(idx);
+                cursor.close();
+            }
+
+            cursor.close();
+            this.imgTool = new ImageTool();
+
+            Bitmap imgPerfil = this.imgTool.convertImageToByte(result);
+            Bitmap resized = Bitmap.createScaledBitmap(imgPerfil, 200, 200, true);
+
+            imatgeArray = this.imgTool.getBytes(resized);
+
+            roundedImageView.setImageBitmap(resized);
+
+        }
     }
 
 
